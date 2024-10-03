305c6874144034c28fb9dc25e72d4f4011884931
Muthui7504 <muthui7504@gmail.com>
2024-10-03 13:15:32 +0300

    12:58

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-	const newArray = [];
-	for (const idx of array) {
-		newArray.push(`${appendString}${idx}`);
-	}
+  const arrayEnd = [];
+  for (const idx of array) {
+    arrayEnd.push(`${appendString}${idx}`);
+  }
 
-  return newArray;
+  return arrayEnd;
 }
